32c062aecfa3138927e90a5394690965b9eda4db
Greg Stupp <gstupp@scripps.edu>
2017-01-11 14:22:59 -0800

    update wikidata ginas mapping

--- ginas/map_to_mygene/join_ginas_unii.py
+++ ginas/map_to_mygene/join_ginas_unii.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-ginas = pd.read_csv('fullSeedData-2016-06-16.csv', index_col=0)
-unii = pd.read_csv('unii_records.tsv', sep='\t')
+
+ginas = pd.read_csv('fullSeedData-2016-06-16.csv', index_col=0, low_memory=False)
+
+unii = pd.read_csv('unii_records.tsv', sep='\t', low_memory=False)
+# add suffix onto column names
+unii.columns = unii.columns.map(lambda x: str(x) + '_from_unii')
+unii = unii.rename(columns={'UNII_from_unii': 'UNII'})
+
 df = pd.merge(ginas, unii, on='UNII', how='left')
 df.to_csv('fullSeedData-2016-06-16_UNII.csv')
